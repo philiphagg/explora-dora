99fbcbb71f819b8a2f1264b59fcc96e98b7995fa
MiniMarius <seffermarius@gmail.com>
2021-11-29 21:55:48 +0100

    removed short test of getDistance-function

--- Views/MapPresenterFile.js
+++ Views/MapPresenterFile.js
@@ -32,10 +32,6 @@ function MapPresenterFile() {
             longitude: 18.004100611617844
         }
     ]
-    getDistance(
-        {latitude: 51.5103, longitude: 7.49347},
-        {latitude: "51° 31' N", longitude: "7° 28' E"}
-    );
 
     React.useEffect(() => {
         (async () => {
