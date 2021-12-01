9e4bf9d1a4b25d33efb489d482666d70a28b7ebd
MiniMarius <seffermarius@gmail.com>
2021-12-01 14:29:57 +0100

    dark mode toggle now working with map

--- Views/MapPresenterFile.js
+++ Views/MapPresenterFile.js
@@ -23,6 +23,7 @@ function MapPresenterFile() {
         dispatch(getMarkers())
     }, []);
     const dispatch = useDispatch();
+    const theme = useSelector((state) => state.theme.value.theme);
 
     React.useEffect(() => {
         (async () => {
@@ -75,7 +76,7 @@ function MapPresenterFile() {
                 >
                     {/* Shows behind the mask, you can put anything here, such as an image */}
                     <MapView region={location} showsUserLocation={true}
-                             provider={PROVIDER_GOOGLE} style={styles.map} customMapStyle={customMap}
+                             provider={PROVIDER_GOOGLE} style={styles.map} customMapStyle={theme.dark ? darkMap : lightMap}
                              scrollEnabled={false}
                              zoomEnabled={false} rotateEnabled={false} pitchEnabled={false}>
 
@@ -86,6 +87,7 @@ function MapPresenterFile() {
                             }} onPress={() => {
                                 if (getDistance(marker, location) > 15) {
                                     console.log("Marker is too far away")
+                                    console.log(theme)
                                 } else {
                                     dispatch(handleRemoveItem({name: marker.name}))
                                     console.log("Marker near you clicked")
@@ -99,7 +101,7 @@ function MapPresenterFile() {
     );
 }
 
-const customMap = [
+const darkMap = [
     {
         "elementType": "geometry",
         "stylers": [
@@ -312,6 +314,194 @@ const customMap = [
         ]
     }
 ]
+
+const lightMap = [
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d3d3d3"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "color": "#808080"
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#b3b3b3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "weight": 1.8
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#d7d7d7"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ebebeb"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#a7a7a7"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#efefef"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#696969"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#737373"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#d6d6d6"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#dadada"
+            }
+        ]
+    }
+]
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
